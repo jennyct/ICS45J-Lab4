4620ddfbceeed30dccd2279d75a919a64da168ed
Jenny Tang <jennyct@uci.edu>
2015-05-19 13:50:56 -0700

    Declared Constants

---
+++ src/RacingGroup.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+
+public class RacingGroup implements RacingGroupInterface{
+	
+	public RacingGroup () {
+		
+	}
+	
+	@Override
+	public ArrayList<RacingSmiley> getRacers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/RacingSmileyAnimation.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+
+
+public class RacingSmileyAnimation implements RacingAnimationInterface {
+
+	private String fastestSmileyName;
+	private String slowestSmileyName;
+	
+	private int fewestTicks;
+	private int mostTicks;
+	private int averageTicks;
+	
+	private String statisticsTitle;
+	
+	private ArrayList<RacingSmiley> racers;
+	
+	@Override
+	public void animate() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ArrayList<RacingSmiley> getRacers() {
+		return racers;
+	}
+
+	@Override
+	public String getStatisticsTitle() {
+		return statisticsTitle;
+	}
+
+	@Override
+	public double getAverageTicks() {
+		return averageTicks;
+	}
+
+	@Override
+	public int getFewestTicks() {
+		return fewestTicks;
+	}
+
+	@Override
+	public int getMostTicks() {
+		return mostTicks;
+	}
+
+	@Override
+	public String getFastestSmileyName() {
+		return fastestSmileyName;
+	}
+
+	@Override
+	public String getSlowestSmileyName() {
+		return slowestSmileyName;
+	}
+
+}
