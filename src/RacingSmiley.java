facaa8bce9d98549d87d7eec7dcaf67db830039e
Jenny Tang <jennyct@uci.edu>
2015-05-19 13:17:54 -0700

    Declared Constants and Created Constructors

--- src/RacingSmiley.java
+++ src/RacingSmiley.java
@@ -1,4 +1,77 @@
+import java.awt.Color;
+import java.util.Random;
 
-public class RacingSmiley {
+// RacingSmiley.java
+// 
+// ICS 45J : Lab Assignment 4
+
+public class RacingSmiley extends AnimatedSmiley implements RacingSmileyInterface {
+
+	private String smileyName;
+	private Color smileyNameColor;
+	private int currentLap;
+	private int currentDirection;
+	
+	private int ticks;
+	private int pixelsPerTick;
+	
+	private Random generator;
+	
+	
+	public RacingSmiley(AnimatedSmiley orig, String name, Color nameColor) {
+		super(orig);
+		
+		smileyName = name;
+		smileyNameColor = nameColor;
+		generator = new Random();
+		currentLap = 0;
+		currentDirection = 1;
+		ticks = 0;
+		
+		
+	}
+	
+	public RacingSmiley(RacingSmiley orig, String newName, Color newColor) {
+		
+		super(orig);
+		
+		smileyName = newName;
+		smileyNameColor = newColor;
+		generator = new Random();
+		currentLap = 0;
+		currentDirection = 1;
+		ticks = 0;
+	}
+
+	@Override
+	public boolean finishedRace() {
+		return (currentLap == 5) ? true : false;
+	}
+
+	@Override
+	public void raceForOneTick() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getTicks() {
+		return ticks;
+	}
+
+	@Override
+	public String getSmileyName() {
+		return smileyName;
+	}
+
+	@Override
+	public Color getSmileyNameColor() {
+		return smileyNameColor;
+	}
+
+	@Override
+	public int getLapsCompleted() {
+		return currentLap;
+	}
 
 }
