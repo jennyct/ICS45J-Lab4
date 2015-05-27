fec0c3d93ba9370ea2decaff5f039ec499a5ade8
Stephen Em <stphnem@gmail.com>
2015-05-27 12:37:37 -0700

    Added doc and changed the speed adjustment

--- src/RacingSmiley.java
+++ src/RacingSmiley.java
@@ -5,6 +5,11 @@ import java.util.Random;
 // 
 // ICS 45J : Lab Assignment 4
 
+/**
+ * Extended AnimatedSmiley that can perform in races
+ * @author Stephen Em and Jenny Tang
+ *
+ */
 public class RacingSmiley extends AnimatedSmiley implements RacingSmileyInterface {
 
 	private String smileyName;
@@ -19,9 +24,15 @@ public class RacingSmiley extends AnimatedSmiley implements RacingSmileyInterfac
 	private int speedAdjustment;
 
 	public static final int TOTAL_LAPS = 10;
-	public static final int MAX_MOVEMENT = 10;
+	public static final int MAX_MOVEMENT = 15;
 	
 	
+	/**
+	 * The default constructor for a RacingSmiley
+	 * @param orig - animated smiley
+	 * @param name - name of racing smiley
+	 * @param nameColor - color of the name string
+	 */
 	public RacingSmiley(AnimatedSmiley orig, String name, Color nameColor) {
 		super(orig,generator.nextInt(MAX_MOVEMENT)+1,0);
 		
@@ -32,10 +43,15 @@ public class RacingSmiley extends AnimatedSmiley implements RacingSmileyInterfac
 		ticks = 0;
 		baseSpeed = getCurrentXMovement();
 		strategy = generateStrat();
-		speedAdjustment = generator.nextInt(baseSpeed)+3;
-		System.out.println(smileyName + " strategy is " + strategy);
+		speedAdjustment = generator.nextInt(baseSpeed)/2;
 	}
 	
+	/**
+	 * The copy constructor for a RacingSmiley
+	 * @param orig - racing smiley
+	 * @param name - new name of racing smiley
+	 * @param nameColor - new color of the name string
+	 */
 	public RacingSmiley(RacingSmiley orig, String newName, Color newColor) {
 		
 		super(orig,generator.nextInt(MAX_MOVEMENT)+1,0);
@@ -47,20 +63,65 @@ public class RacingSmiley extends AnimatedSmiley implements RacingSmileyInterfac
 		ticks = 0;
 		baseSpeed = getCurrentXMovement();
 		strategy = generateStrat();
-		speedAdjustment = generator.nextInt(baseSpeed)+3;
-		System.out.println(smileyName + " strategy is " + strategy);
+		speedAdjustment = generator.nextInt(baseSpeed)/2;
 	}
 	
+	/**
+	 * Change whether the smiley faces left or right
+	 */
 	public void changeSmileyProfile() {
 		changeEye();
 		changeSmile();
 	}
 	
+	/**
+	 * Swap eye colors
+	 */
+	private void changeEye() {
+		Color tempEyeColor = getLeftEye().getColor();
+		getLeftEye().setColor(getRightEye().getColor());
+		getRightEye().setColor(tempEyeColor);
+	}
+	
+	/**
+	 * Swap position of the smiley to be on the opposite edge
+	 */
+	private void changeSmile() {
+		getSmile().translate((int)((currentDirection == 1) ? getFace().getXLength() : -getFace().getXLength()), 0);
+	}
+	
+	/**
+	 * Generate a strategy for the smiley
+	 * @return an int corresponding to the strategy
+	 * 0 - no change
+	 * 1 - slow up on each lap
+	 * 2 - speed up on each lap
+	 */
+	private int generateStrat() {
+		switch(generator.nextInt(3)) {
+		case 0:
+			return 0;
+		case 1:
+			return 1;
+		case 2:
+			return 2;
+		default:
+			return -1;
+		}
+	}
+	
+	/**
+	 * Determines if smiley has finished the race
+	 * True if laps are equal to total laps of race, otherwise false
+	 */
 	@Override
 	public boolean finishedRace() {
 		return (currentLap == TOTAL_LAPS) ? true : false;
 	}
 
+	/**
+	 * Move the racing smiley for one tick
+	 */
 	@Override
 	public void raceForOneTick() {
 		moveIt();
@@ -91,10 +152,19 @@ public class RacingSmiley extends AnimatedSmiley implements RacingSmileyInterfac
 		return currentDirection;
 	}
 	
+	/**
+	 * 
+	 * @param direction - direction for smiley to move:
+	 * 1 for right
+	 * -1 for left
+	 */
 	public void setCurrentDirection(int direction) {
 		currentDirection = direction;
 	}
 	
+	/**
+	 * Increase the lap by one
+	 */
 	public void incrementLap() {
 		currentLap++;
 	}
@@ -109,28 +179,5 @@ public class RacingSmiley extends AnimatedSmiley implements RacingSmileyInterfac
 	
 	public int getSpeedAdjustment() {
 		return speedAdjustment;
-	}
-	
-	private void changeEye() {
-		Color tempEyeColor = getLeftEye().getColor();
-		getLeftEye().setColor(getRightEye().getColor());
-		getRightEye().setColor(tempEyeColor);
-	}
-	
-	private void changeSmile() {
-		getSmile().translate((int)((currentDirection == 1) ? getFace().getXLength() : -getFace().getXLength()), 0);
-	}
-	
-	private int generateStrat() {
-		switch(generator.nextInt(3)) {
-		case 0:
-			return 0;
-		case 1:
-			return 1;
-		case 2:
-			return 2;
-		default:
-			return -1;
-		}
 	}
 }
