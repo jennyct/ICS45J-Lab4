6b345cbfa323caca676b070c4b9e1cd368ebc9ff
Stephen Em <stphnem@gmail.com>
2015-05-27 12:06:56 -0700

    Added documentation for racing group

--- src/RacingGroup.java
+++ src/RacingGroup.java
@@ -2,26 +2,54 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.awt.Color;
 
+/** 
+ * The group of smileys that are in the race
+ * @author Stephen Em and Jenny Tang
+ *
+ */
 public class RacingGroup implements RacingGroupInterface{
 	
-	private ArrayList<String> smileyNames = new ArrayList<String>(Arrays.asList("Patricia","Shaunna","Cecily","Genevieve","Chris",
-			"Loree","Williemae","Latia","Adriene","Beata","Lesley","Alvaro","Treasa","Chauncey","Larita","Debby","Ricky","Nicol",
-			"Nick","Neda","Rosemary","Toshiko","Ute","Matt","Ricarda","Marx","Margarito","Mirian","Bettie","Winona"));
-	private ArrayList<Color> nameColors = new ArrayList<Color>(Arrays.asList(Color.BLUE,Color.CYAN,Color.DARK_GRAY,Color.GRAY,Color.GREEN,
-			Color.LIGHT_GRAY,Color.MAGENTA,Color.ORANGE,Color.PINK,Color.RED,Color.WHITE,new Color(102,255,178),new Color(135,80,80),
-			new Color(135,80,120),new Color(93,80,135),new Color(80,126,135),new Color(80,135,99),new Color(113,135,80),new Color(135,100,80),
-			new Color(198,47,47),new Color(193,47,198),new Color(47,67,198),new Color(47,173,198),new Color(47,198,92),new Color(132,198,47),
-			new Color(198,132,47),new Color(242,105,132),new Color(164,105,242),new Color(105,242,164),new Color(178,242,105)));
+	/**
+	 * List of 30 names for smileys
+	 */
+	private ArrayList<String> smileyNames = new ArrayList<String>(Arrays.asList(
+			"Patricia", "Shaunna", "Cecily", "Genevieve", "Chris", "Loree",
+			"Williemae", "Latia", "Adriene", "Beata", "Lesley", "Alvaro",
+			"Treasa", "Chauncey", "Larita", "Debby", "Ricky", "Nicol",
+			"Nick", "Neda", "Rosemary", "Toshiko", "Ute", "Matt", "Ricarda", 
+			"Marx", "Margarito", "Mirian", "Bettie", "Winona"));
+	
+	/**
+	 * List of 30 colors for smileys
+	 */
+	private ArrayList<Color> nameColors = new ArrayList<Color>(Arrays.asList(
+			Color.BLUE, Color.CYAN, Color.DARK_GRAY, Color.GRAY, Color.GREEN,
+			Color.LIGHT_GRAY, Color.MAGENTA, Color.ORANGE, Color.PINK, 
+			Color.RED, Color.WHITE, new Color(102,255,178), 
+			new Color(135,80,80), new Color(135,80,120), new Color(93,80,135),
+			new Color(80,126,135), new Color(80,135,99), new Color(113,135,80),
+			new Color(135,100,80), new Color(198,47,47), new Color(193,47,198),
+			new Color(47,67,198), new Color(47,173,198), new Color(47,198,92),
+			new Color(132,198,47),new Color(198,132,47), new Color(242,105,132),
+			new Color(164,105,242), new Color(105,242,164), new Color(178,242,105)));
+	
+	/**
+	 * Number of smileys generated for the race
+	 * Change this number to increase amount of smileys
+	 */
 	private static final int NUMBER_OF_SMILEYS = 5;
 	private ArrayList<RacingSmiley> racingSmileys;
 	
-	
-	// The constructor builds an ArrayList of racers
+	/** The constructor builds an ArrayList of racers
 	// all racing smileys, each with the same shape and
 	// color, with mouths the same color as the background
 	// of the screen (the passed-in parameter), moving to
 	// the right to start, but each with a different name
 	// and name color
+	 * 
+	 * @param background - background color
+	 */
+
 	public RacingGroup(Color background) {
 		racingSmileys = new ArrayList<RacingSmiley>();
 		int smileyBuffer = (400/NUMBER_OF_SMILEYS);
@@ -44,6 +72,9 @@ public class RacingGroup implements RacingGroupInterface{
 		}
 	}
 	
+	/**
+	 * Access the group of smileys
+	 */
 	@Override
 	public ArrayList<RacingSmiley> getRacers() {
 		return racingSmileys;
